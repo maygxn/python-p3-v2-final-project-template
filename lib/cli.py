2124eb7d61aeecebb7f254a6f0a48a7186854095
Meagan Truong <MeaganTruong1@yahoo.com>
2024-01-03 10:42:01 -0600

    added back menu_items

--- lib/cli.py
+++ lib/cli.py
@@ -1,10 +1,23 @@
 # lib/cli.py
-
+from menu_items import Menu
 from helpers import (
     exit_program,
     helper_1
 )
-
+# create menu items
+menu_items = [
+    Menu("Loaded Jalapeno Cheese Bytes", 10),
+    Menu("Baked Feta Bytes", 8),
+    Menu("Crab Cake Bytes",11),
+    Menu("Byte Smash Burger Combo",12),
+    Menu("Byte Smash Burger w/ Cheese Combo",13),
+    Menu("Spicy Chicken Sandwich Combo",11),
+    Menu("Chicken Tenders Combo",10),
+    Menu("Bacon Byte Burger Combo",14),
+    Menu("Southwest Salad",12),
+    Menu("Soft Drink",3),
+    Menu("Lemonade",3)
+]
 
 def main():
     while True:
@@ -13,7 +26,7 @@ def main():
         if choice == "0":
             exit_program()
         elif choice == "1":
-            helper_1()
+            view_menu()
         elif choice == "2":
             print("helper func 2 - order food")
         elif choice == "3":
@@ -23,7 +36,6 @@ def main():
         else:
             print("Invalid choice")
 
-
 def menu():
     print("Please select an option:")
     print("0. Exit the program")
@@ -31,6 +43,11 @@ def menu():
     print("2. Order Food")
     print("3. View all orders")
     print("4. Reorder")
+
+def view_menu():
+    print("Menu:")
+    for item in menu_items:
+        print(f"{item.name}: ${item.price}")
 
 if __name__ == "__main__":
     main()
--- lib/debug.py
+++ lib/debug.py
@@ -2,7 +2,8 @@
 # lib/debug.py
 
 from models.__init__ import CONN, CURSOR
+from menu_item import Menu
 import ipdb
 
-menu(cheeseburger, 10)
+cheeseburger = Menu("Cheeseburger", 10)
 ipdb.set_trace()
